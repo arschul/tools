5bb9bc19f11186a95f3b7f03d1b7c688c4bf255d
arschul <andersonschultz01@gmail.com>
2026-07-04 21:34:05 -0300

    Add convos-a2.js for Dialogue Player tool

---
+++ dialogue-player/convos-a2.js
@@ -0,0 +1,304 @@
+const CONVOS_A2 = [
+  {
+    "id": 1,
+    "level": "A2",
+    "p1": "I can't find my [object] anywhere. Have you seen it?",
+    "p2": "I saw it on your desk this morning."
+  },
+  {
+    "id": 2,
+    "level": "A2",
+    "p1": "What did you do last weekend?",
+    "p2": "I went to [place] with my [family]."
+  },
+  {
+    "id": 3,
+    "level": "A2",
+    "p1": "Do you want to try [food] for lunch?",
+    "p2": "Sure, I've never had it before."
+  },
+  {
+    "id": 4,
+    "level": "A2",
+    "p1": "It's really [weather] today, isn't it?",
+    "p2": "I know, I forgot my jacket at home."
+  },
+  {
+    "id": 5,
+    "level": "A2",
+    "p1": "How often do you play [sport]?",
+    "p2": "About twice a week, after school."
+  },
+  {
+    "id": 6,
+    "level": "A2",
+    "p1": "What time does the movie start?",
+    "p2": "I think it starts at [time]."
+  },
+  {
+    "id": 7,
+    "level": "A2",
+    "p1": "Did you finish your [subject] homework?",
+    "p2": "Not yet, I'll do it after dinner."
+  },
+  {
+    "id": 8,
+    "level": "A2",
+    "p1": "My [family] is visiting us this [season].",
+    "p2": "That's nice, will you go to [place] together?"
+  },
+  {
+    "id": 9,
+    "level": "A2",
+    "p1": "I lost my [object] on the bus yesterday.",
+    "p2": "Oh no, did you call the bus company?"
+  },
+  {
+    "id": 10,
+    "level": "A2",
+    "p1": "Do you prefer [food] or [food]?",
+    "p2": "I think I like the first one better."
+  },
+  {
+    "id": 11,
+    "level": "A2",
+    "p1": "What's the weather going to be like tomorrow?",
+    "p2": "The forecast says it will be [weather]."
+  },
+  {
+    "id": 12,
+    "level": "A2",
+    "p1": "I'm thinking about getting a pet [animal].",
+    "p2": "That sounds fun, do you have space for it?"
+  },
+  {
+    "id": 13,
+    "level": "A2",
+    "p1": "Can you recommend a good [movie_genre] movie?",
+    "p2": "Yes, I watched a great one last week."
+  },
+  {
+    "id": 14,
+    "level": "A2",
+    "p1": "How was your trip to [place]?",
+    "p2": "It was amazing, we stayed for a whole week."
+  },
+  {
+    "id": 15,
+    "level": "A2",
+    "p1": "I want to learn [hobby] this year.",
+    "p2": "That's a great goal, I could show you the basics."
+  },
+  {
+    "id": 16,
+    "level": "A2",
+    "p1": "My [family] works as a [job].",
+    "p2": "That must be really interesting work."
+  },
+  {
+    "id": 17,
+    "level": "A2",
+    "p1": "Did you bring your [object] today?",
+    "p2": "Yes, it's in my backpack."
+  },
+  {
+    "id": 18,
+    "level": "A2",
+    "p1": "What are you doing this [day]?",
+    "p2": "I'm going to [place] with some friends."
+  },
+  {
+    "id": 19,
+    "level": "A2",
+    "p1": "I really enjoy [hobby] on weekends.",
+    "p2": "Me too, it helps me relax."
+  },
+  {
+    "id": 20,
+    "level": "A2",
+    "p1": "Is your [family] coming to the party?",
+    "p2": "Yes, she said she'll arrive around [time]."
+  },
+  {
+    "id": 21,
+    "level": "A2",
+    "p1": "What's your favorite thing about [season]?",
+    "p2": "I love how [weather] it gets outside."
+  },
+  {
+    "id": 22,
+    "level": "A2",
+    "p1": "Have you ever played [sport] before?",
+    "p2": "Yes, I used to play it every summer."
+  },
+  {
+    "id": 23,
+    "level": "A2",
+    "p1": "I need to buy a new [object] soon.",
+    "p2": "You could check the shop near [place]."
+  },
+  {
+    "id": 24,
+    "level": "A2",
+    "p1": "Do you know anyone who works as a [job]?",
+    "p2": "Yes, my neighbor does that job."
+  },
+  {
+    "id": 25,
+    "level": "A2",
+    "p1": "What did you have for breakfast?",
+    "p2": "Just some [food] and [drink]."
+  },
+  {
+    "id": 26,
+    "level": "A2",
+    "p1": "Are you free on [day] afternoon?",
+    "p2": "Yes, I don't have any plans then."
+  },
+  {
+    "id": 27,
+    "level": "A2",
+    "p1": "I'm a bit nervous about my [subject] test.",
+    "p2": "Don't worry, you've studied a lot for it."
+  },
+  {
+    "id": 28,
+    "level": "A2",
+    "p1": "Where did you buy your [object]?",
+    "p2": "I got it at a small shop downtown."
+  },
+  {
+    "id": 29,
+    "level": "A2",
+    "p1": "My favorite season is [season], what about you?",
+    "p2": "I actually prefer the opposite one."
+  },
+  {
+    "id": 30,
+    "level": "A2",
+    "p1": "Do you usually drink [drink] in the morning?",
+    "p2": "Yes, it helps me wake up."
+  },
+  {
+    "id": 31,
+    "level": "A2",
+    "p1": "What movie are we watching tonight?",
+    "p2": "I picked a [movie_genre] one, I hope that's ok."
+  },
+  {
+    "id": 32,
+    "level": "A2",
+    "p1": "I saw your [family] at [place] yesterday.",
+    "p2": "Oh really? She didn't mention that to me."
+  },
+  {
+    "id": 33,
+    "level": "A2",
+    "p1": "Can you pass me the [object] on the table?",
+    "p2": "Sure, here you go."
+  },
+  {
+    "id": 34,
+    "level": "A2",
+    "p1": "How long have you been doing [hobby]?",
+    "p2": "About two years now."
+  },
+  {
+    "id": 35,
+    "level": "A2",
+    "p1": "What's the best [food] place near here?",
+    "p2": "There's a great little restaurant downtown."
+  },
+  {
+    "id": 36,
+    "level": "A2",
+    "p1": "I think it might rain later today.",
+    "p2": "Yeah, it's already looking pretty [weather]."
+  },
+  {
+    "id": 37,
+    "level": "A2",
+    "p1": "Did your team win the [sport] game?",
+    "p2": "No, but it was really close."
+  },
+  {
+    "id": 38,
+    "level": "A2",
+    "p1": "I'm thinking of becoming a [job] someday.",
+    "p2": "That sounds like a great career choice."
+  },
+  {
+    "id": 39,
+    "level": "A2",
+    "p1": "Do you remember where you left your [object]?",
+    "p2": "I think I left it at [place]."
+  },
+  {
+    "id": 40,
+    "level": "A2",
+    "p1": "What time should we meet on [day]?",
+    "p2": "Let's say around [time], is that ok?"
+  },
+  {
+    "id": 41,
+    "level": "A2",
+    "p1": "I really want a [color] jacket for winter.",
+    "p2": "That would look great on you."
+  },
+  {
+    "id": 42,
+    "level": "A2",
+    "p1": "How's your [family] doing these days?",
+    "p2": "She's doing well, thanks for asking."
+  },
+  {
+    "id": 43,
+    "level": "A2",
+    "p1": "Do you take [subject] class this semester?",
+    "p2": "Yes, it's actually one of my favorites."
+  },
+  {
+    "id": 44,
+    "level": "A2",
+    "p1": "I forgot my [object] at [place] again.",
+    "p2": "You should really make a checklist."
+  },
+  {
+    "id": 45,
+    "level": "A2",
+    "p1": "What kind of [animal] do you want to have?",
+    "p2": "I've always wanted a small one, like a [animal]."
+  },
+  {
+    "id": 46,
+    "level": "A2",
+    "p1": "Are you excited for the [season] holidays?",
+    "p2": "Definitely, I love that time of year."
+  },
+  {
+    "id": 47,
+    "level": "A2",
+    "p1": "Let's grab some [drink] before class.",
+    "p2": "Good idea, I need something warm."
+  },
+  {
+    "id": 48,
+    "level": "A2",
+    "p1": "Did you watch the [movie_genre] movie last night?",
+    "p2": "Yes, it was better than I expected."
+  },
+  {
+    "id": 49,
+    "level": "A2",
+    "p1": "What's the plan for [day] evening?",
+    "p2": "We're meeting at [place] around [time]."
+  },
+  {
+    "id": 50,
+    "level": "A2",
+    "p1": "Do you think you'll play [sport] again this [season]?",
+    "p2": "Yeah, I'm actually looking forward to it."
+  }
+];
+
+if (typeof module !== 'undefined' && module.exports) module.exports = CONVOS_A2;
